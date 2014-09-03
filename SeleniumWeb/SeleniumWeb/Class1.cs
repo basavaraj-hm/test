82cd12757417f9404d94fddf8e538445819fe366
basavaraj-hm <basavaraj_hm@hotmail.com>
2014-09-03 18:10:47 +0530

    Simple Testcase

---
+++ SeleniumWeb/SeleniumWeb/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using NUnit.Framework;
+
+namespace SeleniumWeb
+{
+    [TestFixture]
+    public class Class1
+    {
+        private IWebDriver driver;
+        [SetUp]
+        public void setUp()
+        {
+             driver = new FirefoxDriver();
+        }
+
+        [Test]
+        public void unitTest()
+        {
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("http://www.google.com");
+            driver.FindElement(By.XPath("//div/div/input[@spellcheck='false']")).SendKeys("difference between nunit and junit");
+            driver.FindElement(By.XPath("//div/div/input[@spellcheck='false']")).SendKeys(Keys.Enter);
+            driver.FindElement(By.XPath("//div/ol/div[2]/li[2]/div/h3/a")).Click();
+            System.Threading.Thread.Sleep(10000);
+            
+        }
+
+        [TearDown]
+        public void testClose()
+        {
+            driver.Quit();
+        }
+    }
+}
